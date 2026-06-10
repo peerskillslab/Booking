48de52c2e485633a34e1eb10ea084d2e727bedcf
Simon Schneider <simonschneider@simons-macbook-pro-4.home>
2026-06-10 07:33:26 +0200

    Use PSL_Logo.svg for all logos

--- peer-skills-lab-kurse/src/components/Logo.jsx
+++ peer-skills-lab-kurse/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo({ size = 56 }) {
   return (
     <img
-      src="/images/PSL_Logo.jpeg.png"
+      src="/images/PSL_Logo.svg"
       alt="Peer Skills Lab"
       style={{ width: size, height: size, objectFit: 'contain' }}
     />
